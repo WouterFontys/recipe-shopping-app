db1e9d2940551a7e1aedc5434103b35ad9ce5cdc
Yves Stroucken <yves.stroucken@outlook.com>
2018-10-07 13:11:01 +0200

    expanded recipe model && changed the details template to output the ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,26 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'this is simply a test',
-      'https://www.logolynx.com/images/logolynx/82/829ba7822e43ebe89394d1ecbbf152b7.jpeg'),
-    new Recipe('Another Test Recipe', 'this is simply a test',
-      'https://www.logolynx.com/images/logolynx/82/829ba7822e43ebe89394d1ecbbf152b7.jpeg')
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Buns', 2)
+      ])
   ];
 
   getRecipes() {
