7a16f380475f7edca84c1bd0b8676dcc16e46972
Yves Stroucken <yves.stroucken@outlook.com>
2018-10-28 16:05:36 +0100

    general cleanup

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,8 @@
 import {Ingredient} from '../shared/ingredient.model';
-import {EventEmitter} from '@angular/core';
-import {Recipe} from '../recipes/recipe.model';
+import {Subject} from 'rxjs';
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -16,7 +15,7 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice())
+    this.ingredientsChanged.next(this.ingredients.slice())
   }
 
   addMultiple(ingredients: Ingredient[]){
@@ -31,7 +30,7 @@ export class ShoppingListService {
         this.ingredients.push(clone_deep)
       }
     }
-    this.ingredientsChanged.emit(this.ingredients.slice())
+    this.ingredientsChanged.next(this.ingredients.slice())
   }
 
   clear() {
